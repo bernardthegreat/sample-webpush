82805113a03fa70451a056a597a3aa013342adee
Bernard Gresola <btgresola@uerm.edu.ph>
2020-09-18 13:53:16 +0800

    initial commit

---
+++ precache-manifest.6b8a68ee8f601d2dd451589814da1bab.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "526dd083246dc58e50d0",
+    "url": "/js/about.0f6fa813.js"
+  },
+  {
+    "revision": "320ed7d8039ccad4e09b",
+    "url": "/css/app.c205be66.css"
+  },
+  {
+    "revision": "320ed7d8039ccad4e09b",
+    "url": "/js/app.483f2277.js"
+  },
+  {
+    "revision": "47aeb8d29745a70abe87",
+    "url": "/js/chunk-vendors.d3610baa.js"
+  },
+  {
+    "revision": "44a8ba023fb03b9c7af42eadd82ef6f2",
+    "url": "/img/logo.44a8ba02.png"
+  },
+  {
+    "revision": "6bd4f74f8dc61a187a20bfcc2773545f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9835dc97c8f2dc2731da64d6795765e",
+    "url": "/img/autumn-forest.png"
+  },
+  {
+    "revision": "fd701c3e8bfa613342250e66c139ee08",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8626ae7e998542ada267cb354e38b977",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "99c438421a7fef5361c2794476c1f5f3",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "504f2085009523311760fc93ff7b5c47",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "511b85acf2a54523a06ab7664c3c8c6a",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "2c311ff7f1098917cb7913880aab7988",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "e2f580ddb6d7fb6f006c3a87ebbc0a8d",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "2c311ff7f1098917cb7913880aab7988",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "31e92ecd25f8a846299662d2c185ee8c",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "4f96e9ccda233367a84480d52d5018eb",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "d5b663a30bd9ddd8b462d7a3680b6ffb",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "61292eb3e497d7829272c4dedbbf9d13",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "511b85acf2a54523a06ab7664c3c8c6a",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "620c6811717c9116b18525a2e26c67f6",
+    "url": "/img/icons/plint-badge-96x96.png"
+  },
+  {
+    "revision": "f78c0251d6ddd56ee219a1830ded71b4",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
